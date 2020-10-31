7715c943b4d14df2b668ee920742ef80eb6de05e
BanuTorun <banu.torun@hotmail.com>
2020-10-31 11:42:44 -0500

    no treat or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,9 @@
 public class HalloweenTest {
 
 // I am bout to give everyone candy for halloween
+// I love my family
+// Aras, Tulpar and Ismail
 
+// there will be treat and trick this year
+// stay home and be safe and wear your mask outside
 }
